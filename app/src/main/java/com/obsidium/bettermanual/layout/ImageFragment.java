960ddb2055ba00109ed255932df27c7490a22ec8
KillerInk <KillerInk@email.de>
2019-08-22 17:11:13 +0200

    move navigation for image_fragment to centerwheel. press enter button to en/disable zoom mode, inside zoom navigate with left/right/top/bottom

--- app/src/main/java/com/obsidium/bettermanual/layout/ImageFragment.java
+++ app/src/main/java/com/obsidium/bettermanual/layout/ImageFragment.java
@@ -3,8 +3,11 @@ package com.obsidium.bettermanual.layout;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.util.Log;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.obsidium.bettermanual.ActivityInterface;
 import com.obsidium.bettermanual.MainActivity;
@@ -25,11 +28,15 @@ public class ImageFragment extends BaseLayout {
     private float scaleFactor = 1;
     private final float scaleStep = 0.2f;
     private final float maxScaleFactor = 8;
+    private TextView zoomImageView;
+    private boolean zoomEnabled = false;
 
     public ImageFragment(Context context,ActivityInterface activityInterface) {
         super(context,activityInterface);
         inflateLayout(R.layout.image_fragment);
         surfaceViewParent = (FrameLayout) findViewById(R.id.surfaceParentView);
+        zoomImageView = (TextView)findViewById(R.id.iv_zoom);
+        zoomImageView.setVisibility(GONE);
         imageView = new OptimizedImageView(getContext());
         surfaceViewParent.addView(imageView);
         imageView.setDisplayPosition(new Point(0,0), OptimizedImageView.PositionType.POS_TYPE_NONE);
@@ -117,6 +124,39 @@ public class ImageFragment extends BaseLayout {
     }
 
 
+    public void moveX(boolean z) {
+        Log.d(TAG,"Change translate. Start");
+        Point point = new Point();
+        if (z) {
+            point.offset(20, 0);
+        } else {
+            point.offset(-20, 0);
+        }
+        OptimizedImageView.LayoutInfo layoutInfo = this.imageView.getLayoutInfo();
+        if (layoutInfo != null) {
+            Rect rect = layoutInfo.drawSize;
+            this.imageView.translate(point, new Point(rect.height(), rect.height()), OptimizedImageView.TranslationType.TRANS_TYPE_INNER_CENTER);
+            imageView.redraw();
+            Log.d(TAG,"Change translate. End");
+        }
+    }
+
+    public void moveY(boolean z) {
+        Log.d(TAG,"Change translate. Start");
+        Point point = new Point();
+        if (z) {
+            point.offset(0, -20);
+        } else {
+            point.offset(0, 20);
+        }
+        OptimizedImageView.LayoutInfo layoutInfo = this.imageView.getLayoutInfo();
+        if (layoutInfo != null) {
+            Rect rect = layoutInfo.drawSize;
+            this.imageView.translate(point, new Point(rect.width(), rect.height()), OptimizedImageView.TranslationType.TRANS_TYPE_INNER_CENTER);
+            imageView.redraw();
+            Log.d(TAG,"Change translate. End");
+        }
+    }
 
 
     @Override
@@ -126,32 +166,57 @@ public class ImageFragment extends BaseLayout {
 
     @Override
     public boolean onLowerDialChanged(int value) {
-        if (value > 0) {
-            scaleFactor += scaleStep;
-            if (scaleFactor > maxScaleFactor)
-                scaleFactor = maxScaleFactor;
+        if (zoomEnabled) {
+            if (value > 0) {
+                scaleFactor += scaleStep;
+                if (scaleFactor > maxScaleFactor)
+                    scaleFactor = maxScaleFactor;
 
+            } else {
+                scaleFactor -= scaleStep;
+                if (scaleFactor < 1)
+                    scaleFactor = 1;
+            }
+            OptimizedImageView.LayoutInfo info = imageView.getLayoutInfo();
+            if (info != null) {
+                Point mTranslateDenom = new Point(info.imageSize.width(), info.imageSize.height());
+                Point mTranslate = new Point(info.clipSize.centerX(), info.clipSize.centerY());
+
+                imageView.setScale(scaleFactor, OptimizedImageView.BoundType.BOUND_TYPE_LONG_EDGE);
+                info = imageView.getLayoutInfo();
+
+                final int width2 = info.imageSize.width();
+                final int height = info.imageSize.height();
+                final Point point = new Point(info.clipSize.centerX() - width2 * mTranslate.x / mTranslateDenom.x, info.clipSize.centerY() - height * mTranslate.y / mTranslateDenom.y);
+                imageView.translate(point, new Point(width2, height), OptimizedImageView.TranslationType.TRANS_TYPE_INNER_CENTER);
+                imageView.redraw();
+            }
         }
-        else {
-            scaleFactor -= scaleStep;
-            if (scaleFactor < 1)
-                scaleFactor = 1;
-        }
-        OptimizedImageView.LayoutInfo info = imageView.getLayoutInfo();
-        if (info != null) {
-            Point mTranslateDenom = new Point(info.imageSize.width(), info.imageSize.height());
-            Point mTranslate = new Point(info.clipSize.centerX(), info.clipSize.centerY());
-
-            imageView.setScale(scaleFactor, OptimizedImageView.BoundType.BOUND_TYPE_LONG_EDGE);
-            info = imageView.getLayoutInfo();
-
-            final int width2 = info.imageSize.width();
-            final int height = info.imageSize.height();
-            final Point point = new Point(info.clipSize.centerX() - width2 * mTranslate.x / mTranslateDenom.x, info.clipSize.centerY() - height * mTranslate.y / mTranslateDenom.y);
-            imageView.translate(point, new Point(width2, height), OptimizedImageView.TranslationType.TRANS_TYPE_INNER_CENTER);
-            imageView.redraw();
+        else
+        {
+            if (value < 0)
+            {
+                activityInterface.getAvIndexManager().moveToPrevious();
+            }
+            else
+                activityInterface.getAvIndexManager().moveToNext();
+            loadOptimizedImg();
         }
         return false;
+    }
+
+    private void toggleZoom()
+    {
+        if (zoomEnabled)
+        {
+            zoomEnabled = false;
+            zoomImageView.setVisibility(GONE);
+        }
+        else
+        {
+            zoomEnabled = true;
+            zoomImageView.setVisibility(VISIBLE);
+        }
     }
 
     @Override
@@ -160,7 +225,12 @@ public class ImageFragment extends BaseLayout {
     }
 
     @Override
-    public boolean onUpKeyUp() {
+    public boolean onUpKeyUp()
+    {
+        if (zoomEnabled)
+        {
+            moveY(false);
+        }
         return false;
     }
 
@@ -171,6 +241,10 @@ public class ImageFragment extends BaseLayout {
 
     @Override
     public boolean onDownKeyUp() {
+        if (zoomEnabled)
+        {
+            moveY(true);
+        }
         return false;
     }
 
@@ -181,8 +255,14 @@ public class ImageFragment extends BaseLayout {
 
     @Override
     public boolean onLeftKeyUp() {
-        activityInterface.getAvIndexManager().moveToPrevious();
-        loadOptimizedImg();
+        if (zoomEnabled)
+        {
+            moveX(true);
+        }
+        else {
+            activityInterface.getAvIndexManager().moveToPrevious();
+            loadOptimizedImg();
+        }
         return false;
     }
 
@@ -193,8 +273,14 @@ public class ImageFragment extends BaseLayout {
 
     @Override
     public boolean onRightKeyUp() {
-        activityInterface.getAvIndexManager().moveToNext();
-        loadOptimizedImg();
+        if (zoomEnabled)
+        {
+            moveX(false);
+        }
+        else {
+            activityInterface.getAvIndexManager().moveToNext();
+            loadOptimizedImg();
+        }
         return false;
     }
 
@@ -205,6 +291,7 @@ public class ImageFragment extends BaseLayout {
 
     @Override
     public boolean onEnterKeyUp() {
+        toggleZoom();
         return false;
     }
 
